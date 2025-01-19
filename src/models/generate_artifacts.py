4086d075bdb98118ee8a476de0bf902928efccdc
mazzacolussi <mazzacolussi@gmail.com>
2025-01-19 15:30:25 -0300

    script artefatos

--- src/models/generate_artifacts.py
+++ src/models/generate_artifacts.py
@@ -1,102 +1,191 @@
 import os
-import shutil
 import pickle
 import logging
-
+import click 
 from utils.transformers import CriaFeatures, Json2DF
 from sklearn.pipeline import Pipeline
 
 import warnings
 warnings.filterwarnings('ignore')
 
-def main():
+
+@click.command()
+@click.option('--clf_mode', default=True, help='Define se os artefatos exportados serão do modelo de classificação ou regressão', type=bool)
+def main(clf_mode):
     """Cria os binários do modelo no formato do deploy."""
 
     logger = logging.getLogger(__name__)
-
-    logger.info('Lendo binários')
+    
+    logger.info('Lendo binários.')
 
     json_to_df = Json2DF()
-
-    seletor_1 = pickle.load(
-        open('models/encoders/seletor_1.pkl', 'rb')
-    )
-
     cria_features = CriaFeatures()
 
-    seletor_2 = pickle.load(
-        open('models/encoders/seletor_2.pkl', 'rb')
-    )
+    if clf_mode:
+        encoders_path_folder = 'models/encoders'
 
-    fill_null = pickle.load(
-        open('models/encoders/features_fill_null.pkl', 'rb')
-    )
+        logger.info('Iniciando a criação da pipeline do modelo de classificação.')
 
-    bool_handler = pickle.load(
-        open('models/encoders/bool_handler.pkl', 'rb')
-    )
-
-    fill_string_missing = pickle.load(
-        open('models/encoders/fill_string_missing.pkl', 'rb')
-    )
-
-    normalize_lower_string = pickle.load(
-        open('models/encoders/normalize_lower_string.pkl', 'rb')
-    )
-
-    grouper_object_exists = os.path.exists(os.path.join('models', 'encoders', 'grouper.pkl'))
-    if grouper_object_exists:
-        grouper = pickle.load(
-            open('models/encoders/grouper.pkl', 'rb')
+        seletor_1 = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'seletor_1.pkl'), 'rb')
         )
 
-    encoder = pickle.load(
-        open('models/encoders/encoder.pkl', 'rb')
-    )
+        seletor_2 = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'seletor_1.pkl'), 'seletor_2.pkl', 'rb')
+        )
 
-    conversor_float = pickle.load(
-        open('models/encoders/conversor_float.pkl', 'rb')
-    )
+        fill_null = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'features_fill_null.pkl'), 'rb')
+        )
 
-    logger.info('Sucesso! Binários lidos')
+        bool_handler = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'bool_handler.pkl'), 'rb')
+        )
 
-    for model_file in os.listdir('models/predictors'):
-        if model_file.startswith('model') and model_file.endswith('.pkl'):
-            nome_modelo = model_file[len('model_'):-len('.pkl')]
+        fill_string_missing = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}','fill_string_missing.pkl'), 'rb')
+        )
 
-            modelo = pickle.load(
-                open(os.path.join('models', 'predictors', model_file), 'rb')
+        normalize_lower_string = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'normalize_lower_string.pkl'), 'rb')
+        )
+
+        grouper_object_exists = os.path.exists(os.path.join(f'{encoders_path_folder}', 'grouper.pkl'))
+        if grouper_object_exists:
+            grouper = pickle.load(
+                open(os.path.join(f'{encoders_path_folder}','grouper.pkl'), 'rb')
             )
 
-            logger.info(f'Criando pipeline do modelo {nome_modelo}')
+        encoder = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}','encoder.pkl'), 'rb')
+        )
 
-            pipeline_list = [
-                ('json_to_df', json_to_df),
-                ('seletor_1', seletor_1),
-                ('cria_features', cria_features),
-                ('seletor_2', seletor_2),
-                ('fill_null', fill_null),
-                ('bool_encoder', bool_handler),
-                ('fill_string_missing', fill_string_missing),
-                ('normalize_lower_string', normalize_lower_string),
-                #('agrupador', grouper),
-                ('encoder', encoder),
-                ('conversor_float', conversor_float),
-                ('seletor_3', seletor_2),
-                ('modelo', modelo)
-            ]
+        conversor_float = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}','conversor_float.pkl'), 'rb')
+        )
 
-            if grouper_object_exists:
-                pipeline_list.insert(pipeline_list.index(('encoder', encoder)), ('agrupador', grouper))
+        logger.info('Sucesso! Binários lidos.')
 
-            pipeline_prod = Pipeline(steps=pipeline_list)
+        for model_file in os.listdir('models/predictors'):
+            if model_file.startswith('model_clf') and model_file.endswith('.pkl'):
+                nome_modelo = model_file[len('model_'):-len('.pkl')]
 
-            logger.info(f'Exportando o modelo {nome_modelo} final')
+                modelo = pickle.load(
+                    open(os.path.join('models', 'predictors', model_file), 'rb')
+                )
 
-            pickle.dump(
-                pipeline_prod,
-                open(os.path.join('models', 'wrapped', f'model_{nome_modelo}_pipeline_prod.pkl'), 'wb')
+                logger.info(f'Criando pipeline do modelo {nome_modelo}')
+
+                pipeline_list = [
+                    ('json_to_df', json_to_df),
+                    ('seletor_1', seletor_1),
+                    ('cria_features', cria_features),
+                    ('seletor_2', seletor_2),
+                    ('fill_null', fill_null),
+                    ('bool_encoder', bool_handler),
+                    ('fill_string_missing', fill_string_missing),
+                    ('normalize_lower_string', normalize_lower_string),
+                    #('agrupador', grouper),
+                    ('encoder', encoder),
+                    ('conversor_float', conversor_float),
+                    ('seletor_3', seletor_2),
+                    ('modelo', modelo)
+                ]
+
+                if grouper_object_exists:
+                    pipeline_list.insert(pipeline_list.index(('encoder', encoder)), ('agrupador', grouper))
+
+                pipeline_prod = Pipeline(steps=pipeline_list)
+
+                logger.info(f'Exportando o modelo {nome_modelo} final')
+
+                pickle.dump(
+                    pipeline_prod,
+                    open(os.path.join('models', 'wrapped', f'model_{nome_modelo}_pipeline_prod.pkl'), 'wb')
+                )
+
+    else:
+        encoders_path_folder = 'models/encoders_reg'
+
+        logger.info('Iniciando a criação da pipeline do modelo de regressão.')
+
+        seletor_1 = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'seletor_1.pkl'), 'rb')
+        )
+
+        seletor_2 = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'seletor_2.pkl'), 'rb')
+        )
+
+        fill_null = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'features_fill_null.pkl'), 'rb')
+        )
+
+        bool_handler = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'bool_handler.pkl'), 'rb')
+        )
+
+        fill_string_missing = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}','fill_string_missing.pkl'), 'rb')
+        )
+
+        normalize_lower_string = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}', 'normalize_lower_string.pkl'), 'rb')
+        )
+
+        grouper_object_exists = os.path.exists(os.path.join(f'{encoders_path_folder}', 'grouper.pkl'))
+        if grouper_object_exists:
+            grouper = pickle.load(
+                open(os.path.join(f'{encoders_path_folder}','grouper.pkl'), 'rb')
             )
+
+        encoder = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}','encoder.pkl'), 'rb')
+        )
+
+        conversor_float = pickle.load(
+            open(os.path.join(f'{encoders_path_folder}','conversor_float.pkl'), 'rb')
+        )
+
+        logger.info('Sucesso! Binários lidos.')
+
+        for model_file in os.listdir('models/predictors'):
+            if model_file.startswith('model_reg') and model_file.endswith('.pkl'):
+                nome_modelo = model_file[len('model_'):-len('.pkl')]
+
+                modelo = pickle.load(
+                    open(os.path.join('models', 'predictors', model_file), 'rb')
+                )
+
+                logger.info(f'Criando pipeline do modelo {nome_modelo}')
+
+                pipeline_list = [
+                    ('json_to_df', json_to_df),
+                    ('seletor_1', seletor_1),
+                    ('cria_features', cria_features),
+                    ('seletor_2', seletor_2),
+                    ('fill_null', fill_null),
+                    ('bool_encoder', bool_handler),
+                    ('fill_string_missing', fill_string_missing),
+                    ('normalize_lower_string', normalize_lower_string),
+                    #('agrupador', grouper),
+                    ('encoder', encoder),
+                    ('conversor_float', conversor_float),
+                    ('seletor_3', seletor_2),
+                    ('modelo', modelo)
+                ]
+
+                if grouper_object_exists:
+                    pipeline_list.insert(pipeline_list.index(('encoder', encoder)), ('agrupador', grouper))
+
+                pipeline_prod = Pipeline(steps=pipeline_list)
+
+                logger.info(f'Exportando o modelo {nome_modelo} final')
+
+                pickle.dump(
+                    pipeline_prod,
+                    open(os.path.join('models', 'wrapped', f'model_{nome_modelo}_pipeline_prod.pkl'), 'wb')
+                )
 
     logger.info('Sucesso! Artefatos exportados!')
 
